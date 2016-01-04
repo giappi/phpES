b6a648291bccfb0c5a1f0f708871c4b3c9360467
giappi <giappaig@gmail.com>
2016-01-04 21:40:07 +0700

    Signed-off-by: giappi <giappaig@gmail.com>

--- API.php
+++ API.php
@@ -1,15 +1,23 @@
 <?php
-    require_once 'data/sqlite3.php';
+
+
+    function loadClasses($dir)
+    {
+        $classes = scandir($dir);
+        foreach ($classes as $class)
+        {
+            if(preg_match("/.class.php$/is", $class))
+            {
+                require_once $dir . "/" . $class;
+            }
+        }
+    }
     
-    $dir_objects = "objects";
-	$classes = scandir($dir_objects);
-	foreach ($classes as $class)
-	{
-		if(preg_match("/.class.php$/is", $class))
-		{
-			require $dir_objects . "/" . $class;
-		}
-	}
+    require_once 'data/sqlite3.php';
+    loadClasses("objects");
+    loadClasses("data");
+    
+
     
 
 
